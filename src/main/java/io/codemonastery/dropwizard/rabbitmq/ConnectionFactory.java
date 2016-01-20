fcf95f3fb693eb0be5a233688aadff38405ba020
Randy Pensinger <rspensinger@gmail.com>
2016-01-19 17:43:31 -0800

    another way to make connection

--- src/main/java/io/codemonastery/dropwizard/rabbitmq/ConnectionFactory.java
+++ src/main/java/io/codemonastery/dropwizard/rabbitmq/ConnectionFactory.java
@@ -1,24 +1,30 @@
 package io.codemonastery.dropwizard.rabbitmq;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.health.HealthCheckRegistry;
 import com.google.common.base.Optional;
 import com.rabbitmq.client.Connection;
+import io.dropwizard.lifecycle.setup.LifecycleEnvironment;
 import io.dropwizard.setup.Environment;
 
+import javax.annotation.Nullable;
+import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeoutException;
 import java.util.function.Supplier;
 
 /**
  * For documentation about these configurations, see {@link com.rabbitmq.client.ConnectionFactory}.
- * 
+ *
  * Registers a health check, manages the rabbitmq connection, and adds metrics.
- * 
+ *
  * Note that automaticRecoveryEnabled and topologyRecoveryEnabled are not exposed because they are assumed to be true.
  */
 public class ConnectionFactory extends ConnectionConfiguration {
-    
+
     private ConnectionMetrics metrics;
-    
+
     public ConnectionFactory customeMetrics(ConnectionMetrics metrics){
         this.metrics = metrics;
         return this;
@@ -35,11 +41,34 @@ public class ConnectionFactory extends ConnectionConfiguration {
     public Connection build(final Environment env,
                             final ExecutorService deliveryExecutor,
                             final String name) throws Exception {
+        final HealthCheckRegistry healthChecks = env.healthChecks();
+        final LifecycleEnvironment lifecycle = env.lifecycle();
+        final MetricRegistry metrics = env.metrics();
+        return build(healthChecks, lifecycle, metrics, deliveryExecutor, name);
+    }
+
+    /**
+     * Synchronously connect to rabbitmq, will throw exception to fail if initial connection is unsuccessful.
+     * @param healthChecks health check registry, nullable
+     * @param lifecycle lifcycle, nullable
+     * @param metrics metric registry, nullable
+     * @param deliveryExecutor the executor used by rabbitmq client to deliver messages
+     * @param name name of rabbitmq connection
+     * @throws IOException
+     * @throws TimeoutException
+     */
+    public Connection build(@Nullable HealthCheckRegistry healthChecks,
+                            @Nullable LifecycleEnvironment lifecycle,
+                            @Nullable MetricRegistry metrics,
+                            ExecutorService deliveryExecutor,
+                            String name) throws IOException, TimeoutException {
         final com.rabbitmq.client.ConnectionFactory connectionFactory = makeConnectionFactory();
-        final ConnectionMetrics connectionMetrics = Optional.fromNullable(metrics)
-                .or(() -> new DefaultConnectionMetrics(name, env.metrics()));
+        final ConnectionMetrics connectionMetrics = Optional.fromNullable(this.metrics)
+                .or(() -> {
+                    return new DefaultConnectionMetrics(name, metrics);
+                });
         final Connection connection = connectionFactory.newConnection(deliveryExecutor);
-        registerWithEnvironment(env, name, () -> connection);
+        registerWithEnvironment(healthChecks, lifecycle, () -> connection, name);
         return new WrappedConnectionMetrics(connectionMetrics).wrap(connection);
     }
 
@@ -60,7 +89,7 @@ public class ConnectionFactory extends ConnectionConfiguration {
                 .scheduledExecutorService(name + "-initial-connect-thread")
                 .threads(1)
                 .build();
-        
+
         final ConnectionMetrics connectionMetrics = Optional.fromNullable(metrics)
                 .or(() -> new DefaultConnectionMetrics(name, env.metrics()));
         final WrappedConnectionMetrics connectionMetricsWrapper = new WrappedConnectionMetrics(connectionMetrics);
@@ -69,12 +98,16 @@ public class ConnectionFactory extends ConnectionConfiguration {
             callback.connected(metricsConnection);
         };
         final ConnectAsync connectAsync = new ConnectAsync(connectionFactory, deliveryExecutor, name, initialConnectExecutor, callbackWithMetrics);
-        registerWithEnvironment(env, name, connectAsync::getConnection);
+        registerWithEnvironment(env.healthChecks(), env.lifecycle(), connectAsync::getConnection, name);
         connectAsync.run();
     }
-    
-    private void registerWithEnvironment(final Environment env, final String name, final Supplier<Connection> connection){
-        env.healthChecks().register(name, new ConnectionHealthCheck(connection));
-        env.lifecycle().manage(new ManageConnection(connection));
+
+    private void registerWithEnvironment(HealthCheckRegistry healthChecks, LifecycleEnvironment lifecycle, final Supplier<Connection> connection, final String name){
+        if (healthChecks != null) {
+            healthChecks.register(name, new ConnectionHealthCheck(connection));
+        }
+        if(lifecycle != null){
+            lifecycle.manage(new ManageConnection(connection));
+        }
     }
 }
