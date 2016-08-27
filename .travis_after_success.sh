f56dbec0f99022333f086b742b74dd39af1def33
Randy Pensinger <rspensinger@gmail.com>
2016-08-26 23:30:40 -0700

    fix build step

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -5,6 +5,6 @@ mvn -B jacoco:report coveralls:report
 version_match=`perl -e "print '$TRAVIS_TAG' =~ /^dropwizard-rabbitmq-\d+\.\d+\.\d+\.\d+$/"`
 if [[ "$version_match" == "1" ]]; then
     mvn versions:set -DnewVersion=$TRAVIS_TAG
-    mvn clean deploy --settings release-settings.xml -DskipTests=true -B
+    mvn clean deploy --settings .travis/release-settings.xml -DskipTests=true -B
     curl -X POST --data-urlencode "payload={ \"text\": \"${TRAVIS_TAG} has been released\"}" "${slackrelease}"
 fi
