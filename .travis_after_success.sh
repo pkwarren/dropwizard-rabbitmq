b09ae4fb14f6e1adcf2c7c07208e9595c732c9f8
Randy Pensinger <rspensinger@gmail.com>
2016-08-26 21:41:19 -0700

    perhaps this will work

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -5,5 +5,5 @@ mvn -B jacoco:report #coveralls:report
 version_match=`perl -e "print '$TRAVIS_TAG' =~ /^dropwizard-rabbitmq-\d+\.\d+\.\d+\.\d+$/"`
 if [[ "$version_match" == "1" ]]; then
     mvn versions:set -DnewVersion=$TRAVIS_TAG
-    mvn clean deploy -DskipTests=true -B
+    mvn clean deploy --settings release-settings.xml -DskipTests=true -B
 fi
