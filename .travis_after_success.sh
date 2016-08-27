612b393f07e089ed25ac2571628bfbfab0e31d00
Randy Pensinger <rspensinger@gmail.com>
2016-08-26 19:23:06 -0700

    try to setup automated releases

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 mvn -B jacoco:report #coveralls:report
+
+if [[ -n ${TRAVIS_TAG} ]]; then
+    echo "Skipping deployment for tag \"${TRAVIS_TAG}\""
+    exit
+fi
+
+if [[ ${TRAVIS_BRANCH} != 'master' ]]; then
+    echo "Skipping deployment for branch \"${TRAVIS_BRANCH}\""
+    exit
+fi
+
+if [[ "$TRAVIS_PULL_REQUEST" = "true" ]]; then
+    echo "Skipping deployment for pull request"
+    exit
+fi
+
+mvn clean release:clean release:prepare -B
+mvn release:perform
